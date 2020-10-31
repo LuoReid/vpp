1c37bcd58f6c221b90f0f7fccfce3ed79856903d
luo.reid <luo.reid@gmail.com>
2020-10-31 18:10:26 +0800

    feat: add moment

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
+import moment from 'moment'
+import 'moment/locale/en-au'
+moment.locale('en-au');
+
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+Vue.use(Antd);
+
 new Vue({
   render: h => h(App)
 }).$mount("#app");
